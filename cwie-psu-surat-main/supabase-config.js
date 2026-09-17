475fa65a90302d0c57f11c26e61e3306861da0d1
kannikart-afk <kannikar.t@psu.ac.th>
2026-09-17 09:34:57 +0700

    Update supabase-config.js

--- cwie-psu-surat-main/supabase-config.js
+++ cwie-psu-surat-main/supabase-config.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = 'https://eiagqhekxczwkhpfeesl.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_3sLobY_gHEJ0KCxXElSegQ_t29cXMnP';
+const SUPABASE_URL = 'https://evbglhmzwjravljcaghs.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_IKyp9TT7Sy5tIh-amskGTQ_zXLtcJ4b';
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
